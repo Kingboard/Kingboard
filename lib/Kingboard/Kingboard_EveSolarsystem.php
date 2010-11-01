81831168d83b4886a271ee639e709ac5e2e66cc7
ppetermann <ppetermann80@googlemail.com>
2010-11-01 16:03:05 +0100

    missing file

---
+++ lib/Kingboard/Kingboard_EveSolarsystem.php
@@ -0,0 +1,20 @@
+<?php
+class Kingboard_EveSolarSystem extends King23_MongoObject
+{
+    protected $_className = __CLASS__;
+
+    public static function getById($id)
+    {
+        return self::_getInstanceById(__CLASS__, $id);
+    }
+
+    public static function find($criteria = array())
+    {
+        return self::_find(__CLASS__, $criteria);
+    }
+
+    public static function getBySolarSystemId($solarSystemID)
+    {
+        return self::_getInstanceByCriteria(__CLASS__, array('solarSystemID' => $solarSystemID));
+    }
+} 
