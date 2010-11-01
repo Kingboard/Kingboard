b896e71099c51189f141367442a9954c3c1cba59
ppetermann <ppetermann80@googlemail.com>
2010-11-01 15:56:11 +0100

    working on the homepage

--- tasks/EveImport_Task.php
+++ tasks/EveImport_Task.php
@@ -51,4 +51,43 @@ class EveImport_Task extends King23_CLI_Task
         }
 
     }
+
+    public function solarsystems(array $options)
+    {
+        $this->cli->message('importing solarsystems from sqlite');
+
+        if(count($options) < 1 || !file_exists($options[0]) || !($pdo = new PDO("sqlite:" . $options[0], null,null)))
+        {
+            $this->cli->error('you need to specify a sqlite filename as option');
+            return -1;
+        }
+
+        $query = "
+            SELECT * FROM
+                mapSolarSystems a
+                LEFT JOIN mapRegions b ON a.regionID = b.regionID
+                LEFT JOIN mapConstallations c ON a.constellationID = c.constallationID
+        ";
+        $stmt = $pdo->query($query);
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC))
+        {
+            if(is_null(Kingboard_EveSolarSystem::getBySolarSystemId($row['solarSystemID'])))
+            {
+                $this->cli->message("{$row['solarSystemID']} not found...");
+                $item = new Kingboard_EveItem();
+                foreach($row as $key => $val)
+                {
+                    $item[$key] = $val;
+                }
+                $item->save();
+                $this->cli->positive($row['solarSystemID'] . " saved");
+            } else {
+                $this->cli->warning($row['solarSystemID'] . " allready in database");
+            }
+        }
+
+    }
+
+
 }
