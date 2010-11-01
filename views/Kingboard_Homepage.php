79dbf03c0a28abc007674231005e51b99bc1a85a
ppetermann <ppetermann80@googlemail.com>
2010-11-01 14:33:23 +0100

    working on the homepage

--- views/Kingboard_Homepage.php
+++ views/Kingboard_Homepage.php
@@ -5,7 +5,6 @@ class Kingboard_Homepage extends Kingboard_BaseView
     public function index($request)
     {
         $data = Kingboard_Kill::find()->sort(array('killTime' => -1));
-        $data = print_r($data, true);
         $this->render("index.html", array('data' => $data));
     }
 }
