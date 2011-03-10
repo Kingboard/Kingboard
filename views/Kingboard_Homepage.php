94f8633578c581c37158889d3cc7282b768a9bf4
Peter Petermann <ppetermann80@googlemail.com>
2011-03-10 23:14:43 +0100

    fixing intendation

--- views/Kingboard_Homepage.php
+++ views/Kingboard_Homepage.php
@@ -5,7 +5,7 @@ class Kingboard_Homepage extends Kingboard_BaseView
     public function index($request)
     {
         $data = Kingboard_Kill::find()->sort(array('killTime' => -1))->limit(20);
-	$count = Kingboard_Kill::count();
+	    $count = Kingboard_Kill::count();
         return $this->render("index.html", array('data' => $data, 'count' => $count));
     }
 }
