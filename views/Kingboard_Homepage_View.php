f11dc46244bee129356ffac35b51f865134b917f
Peter Petermann <ppetermann80@googlemail.com>
2011-03-12 22:49:51 +0100

    renaming a few files

---
+++ views/Kingboard_Homepage_View.php
@@ -0,0 +1,14 @@
+<?php
+class Kingboard_Homepage_View extends Kingboard_Base_View
+{
+
+    public function index($request)
+    {
+        $data = Kingboard_Kill::find()->sort(array('killTime' => -1))->limit(20);
+	    $count = Kingboard_Kill::count();
+        
+        $stats = Kingboard_Kill_MapReduce_KillsByShip::find();
+
+        return $this->render("index.html", array('data' => $data, 'count' => $count, 'stats' => $stats));
+    }
+}
