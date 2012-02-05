50d56854340918b87de5ca9290d8c0a350e69105
Michael Karbowiak <karbowiak@gmail.com>
2012-02-05 22:28:55 +0800

    Fixed up the last few MapReduces, so they all work. Shame on you Beansman!

--- model/Kingboard_Kill_MapReduce_LossesByShipByPilot.php
+++ model/Kingboard_Kill_MapReduce_LossesByShipByPilot.php
@@ -19,7 +19,7 @@ class Kingboard_Kill_MapReduce_LossesByShipByPilot extends King23_MongoObject im
                 var info = {};
                 info['group'] = {};
                 info['ship'] = {}
-                info['group'][ship.marketGroup.parentGroup.marketGroupName] = 1;
+                info['group'][ship.marketGroup[0].parentGroup[0].marketGroupName] = 1;
                 info['ship'][this.victim.shipType] = 1;
                 info['total'] = 1;
                 emit(this.victim.characterID, info);
