9c91d941abdab4008613f9eb38279ee01ccfb14c
Peter Petermann <ppetermann80@googlemail.com>
2012-02-14 02:22:53 +0100

    twipsys, now tooltips working again

--- public/js/common.js
+++ public/js/common.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     // enable tabs
     //$('.tabs').tab('show');
 
-
+    $("body").tooltip({
+        selector: "a[rel=twipsy]",
+        placement: "bottom"
+    });
     $('.dropdown-toggle').dropdown();
 });
