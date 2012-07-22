fc589dd36bc58db8113ef4eacd959ca0ccd1be01
Michael Karbowiak <karbowiak@gmail.com>
2012-07-22 14:08:47 +0200

    Apparently missed a few spaces, a private and removal of a function that wasn't needed.. oops

--- src/Kingboard/Lib/EveCentral/Api.php
+++ src/Kingboard/Lib/EveCentral/Api.php
@@ -4,6 +4,7 @@ namespace Kingboard\Lib\EveCentral;
 class Api
 {
     private $itemID;
+    
     public static function getValue($itemID)
     {
         if(empty($itemID))
@@ -14,13 +15,5 @@ class Api
         $simplexml = simplexml_load_string($xml);
         $isk = (float) $simplexml->marketstat->type->sell->median;
         return $isk;
-    }    
-
-    public static function updateDB($itemID, $isk)
-    {
-        // Insert it to mongo
-        $instance = self::getByItemId($itemID);
-        $instance->iskvalue = $isk;
-        $instance->save();
     }
 }
--- src/Kingboard/Lib/EveCentral/Update.php
+++ src/Kingboard/Lib/EveCentral/Update.php
@@ -4,6 +4,8 @@ namespace Kingboard\Lib\EveCentral;
 class Update
 {
     private $itemID;
+    private $isk;
+    
     public static function updateValue($itemID, $isk)
     {
         // Insert it to mongo
