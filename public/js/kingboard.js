4a102470123bdcdb106b80379f8bff661c304b92
Michael Karbowiak <karbowiak@gmail.com>
2012-07-22 10:53:54 +0200

    indentation fixes, i hope anyway

--- public/js/kingboard.js
+++ public/js/kingboard.js
@@ -1,29 +1,35 @@
 // Typeahead search functions..
 $(function(){
-	$("#searchbox").typeahead({
-		source: function(typeahead, query){
- 
-			//clear the old rate limiter
-			clearTimeout($('#typeahead').data('limiter'));
- 
-			//wrap the ajax stuff into a closure
-			var ajax_request = function()
-			{
-				$.ajax({
-					url: '/autocomplete/',
-					type: 'GET',
-					data: '/' + query,
-					dataType: 'JSON',
-					async: true,
-					success: function(data){typeahead.process(data);}
-				});
-			}
- 
-			//start the new timer
-			$('#typeahead').data('limiter', setTimeout(ajax_request, 250));
-		},
-		onselect: function(obj) {
-			$('form[name="search"]').submit();
-		}
-	});
+    $("#searchbox").typeahead({
+        // html stuff
+        source: function(typeahead, query){
+
+            //clear the old rate limiter
+            clearTimeout($("#typeahead").data("limiter"));
+
+            //wrap the ajax stuff into a closure
+            var ajax_request = function()
+            {
+                $.ajax({
+                    url: "/autocomplete/",
+                    type: "GET",
+                    data: "/" + query,
+                    dataType: "JSON",
+                    async: true,
+                    success: function(results){
+                    typeahead.process(results);
+                    }
+                });
+            }
+            
+            //start the new timer
+            $("#typeahead").data("limiter", setTimeout(ajax_request, 250));
+        },
+        onselect: function(obj) {
+            $('form[name="search"]').submit();
+        },
+        highlighter: function(item){
+            return '<ul class="nav nav-list"><li>' + item + "</li></ul>";
+        }
+    });
 });
