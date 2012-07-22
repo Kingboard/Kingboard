c35bd1cf02958afb2d372f325cb229495f9409a5
Peter Petermann <ppetermann80@googlemail.com>
2012-07-22 11:07:51 +0200

    readded config.php-dist moved kingboard.js contents to the former existing common.js

--- public/js/common.js
+++ public/js/common.js
@@ -7,4 +7,32 @@ $(document).ready(function() {
         placement: "bottom"
     });
     $('.dropdown-toggle').dropdown();
+
+
+    $("#searchbox").typeahead({
+        source: function(typeahead, query){
+
+            //clear the old rate limiter
+            clearTimeout($('#typeahead').data('limiter'));
+
+            //wrap the ajax stuff into a closure
+            var ajax_request = function()
+            {
+                $.ajax({
+                    url: '/autocomplete/',
+                    type: 'GET',
+                    data: '/' + query,
+                    dataType: 'JSON',
+                    async: true,
+                    success: function(data){typeahead.process(data);}
+                });
+            }
+
+            //start the new timer
+            $('#typeahead').data('limiter', setTimeout(ajax_request, 250));
+        },
+        onselect: function(obj) {
+            $('form[name="search"]').submit();
+        }
+   });
 });
