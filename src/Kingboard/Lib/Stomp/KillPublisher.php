1a5385ab3f431f5531a4d79fbe085057fc5dabe3
Peter Petermann <ppetermann80@googlemail.com>
2013-01-13 16:54:47 +0100

    added further destinations

--- src/Kingboard/Lib/Stomp/KillPublisher.php
+++ src/Kingboard/Lib/Stomp/KillPublisher.php
@@ -16,12 +16,42 @@ class KillPublisher
     {
         if(is_null(self::$publisherInstance))
             self::$publisherInstance = new Publisher();
-        $destinations = \King23\Core\Registry::getInstance()->stomp['destination_post'];
+        self::$publisherInstance->send(self::getDestinations($kill), json_encode($kill));
+    }
 
-        // post to multiple destinations
-        foreach($destinations as $destination)
-        {
-            self::$publisherInstance->send($destination, json_encode($kill));
-        }
+    /**
+     * get a combination of the configured destinations as well as the automated post
+     * location, such as solarSystem and involved entities
+     * @param array $kill
+     * @return string
+     */
+    private static function getDestinations(array $kill)
+    {
+        $destinations = array();
+
+        $destinations[] = '/topic/location.solarsystem.' . $kill['location']['solarSystem'];
+        $destinations[] = '/topic/location.region.' . $kill['location']['region'];
+
+        foreach($kill['involvedCharacters'] as $characterID)
+            if($characterID > 0)
+                $destinations[] = '/topic/involved.character.' . $characterID;
+
+        foreach($kill['involvedCorporations'] as $corporationID)
+            if($corporationID > 0)
+                $destinations[] = '/topic/involved.corporation.' . $corporationID;
+
+        foreach($kill['involvedFactions'] as $factionID)
+            if($factionID > 0 )
+                $destinations[] = '/topic/involved.faction.' . $factionID;
+
+        foreach($kill['involvedAlliances'] as $allianceID)
+            if($allianceID > 0 )
+                $destinations[] = '/topic/involved.alliance.' . $allianceID;
+
+        $destinations = array_merge(
+            $destinations,
+            \King23\Core\Registry::getInstance()->stomp['destination_post']
+        );
+        return join(',', $destinations);
     }
 }
