f3596618cccaaf37495168f2b426c01b611623ff
Peter Petermann <ppetermann80@googlemail.com>
2013-12-13 19:05:33 +0100

    stomp fetcher now use logger instead of stdout

--- tasks/Kingboard/KingboardTask.php
+++ tasks/Kingboard/KingboardTask.php
@@ -1,11 +1,13 @@
 <?php
 namespace Kingboard;
+
 use King23\Core\Registry;
+use King23\Tasks\King23Task;
 use Kingboard\Lib\Parser\EveAPI;
-use Kingboard\Model\MapReduce\KillsByDay;
+use Kingboard\Model\Kill;
 use Kingboard\Model\MapReduce\KillsByDayByEntity;
 
-class KingboardTask extends \King23\Tasks\King23Task
+class KingboardTask extends King23Task
 {
     /**
      * documentation for the single tasks
@@ -20,8 +22,6 @@ class KingboardTask extends \King23\Tasks\King23Task
      */
     protected $name = "Kingboard";
 
-
-
     /**
      * Experimental task to enable kill processing from queue.
      * @param array $options
@@ -30,10 +30,12 @@ class KingboardTask extends \King23\Tasks\King23Task
     {
         $this->cli->header("Starting Stomp Import");
         $reg = Registry::getInstance();
+        $log = $reg->getLogger();
 
         $stompcfg = $reg->stomp;
         if (is_null($stompcfg) || !is_array($stompcfg)) {
             $this->cli->error("stomp is not configured, see config.php for details");
+            $log->critical("stomp not configured, exiting");
             return;
         }
 
@@ -43,25 +45,39 @@ class KingboardTask extends \King23\Tasks\King23Task
         $destination = $reg->stomp['destination_read'];
 
         // we subscribe with additional parameters
-        $stomp->subscribe($destination, array(
-            "id" => $reg->stomp['dsub_id'], // dsub id, this one should be some unique identifier that identifies your board
-                                            // multiple boards using the same dsub_id will consume each others subscription
-            "persistent" => "true",         // this flag enables the dsub itself
-            "ack" => "client",              // ensure we don't auto-ack (serverside) but have the client acknowledge his subscription,
-            "prefetch-count" => 1
-        ));
+        $stomp->subscribe(
+            $destination,
+            array(
+                "id" => $reg->stomp['dsub_id'],
+                // dsub id, this one should be some unique identifier that identifies your board
+                // multiple boards using the same dsub_id will consume each others subscription
+                "persistent" => "true",
+                // this flag enables the dsub itself
+                "ack" => "client",
+                // ensure we don't auto-ack (serverside) but have the client acknowledge his subscription,
+                "prefetch-count" => 1
+            )
+        );
 
         while (true) {
             try {
-                if(!$stomp->hasFrame())
+
+                if (!$stomp->hasFrame()) {
                     continue;
+                }
+
                 $frame = $stomp->readFrame();
                 if ($frame) {
+
+                    $log->debug("received frame with message-id: " . $frame->headers['message-id']);
+
                     $killdata = json_decode($frame->body, true);
-                    $existing = \Kingboard\Model\Kill::getByKillId($killdata["killID"]);
+                    $existing = Kill::getByKillId($killdata["killID"]);
 
                     if (!is_null($existing)) {
-                        $this->cli->message($frame->headers['message-id'] . '::' . $killdata["killID"] . " kill by killID exists");
+                        $log->debug(
+                            $frame->headers['message-id'] . '::' . $killdata["killID"] . " kill by killID exists"
+                        );
                         $stomp->ack($frame);
                         continue;
                     }
@@ -69,15 +85,17 @@ class KingboardTask extends \King23\Tasks\King23Task
                         $apiParser = new EveAPI();
                         $apiParser->parseKill($killdata);
 
-                        $this->cli->message($frame->headers['message-id'] . '::' . $killdata["killID"] . " saved");
+                        $log->debug($frame->headers['message-id'] . '::' . $killdata["killID"] . " saved");
                         $stomp->ack($frame);
                     } catch (\Exception $e) {
-                        $this->cli->error($frame->headers['message-id'] . "could not be saved, exception: " . $e->getMessage());
+                        $log->error(
+                            $frame->headers['message-id'] . "could not be saved, exception: " . $e->getMessage()
+                        );
                     }
                 }
             } catch (\StompException $e) {
-                $this->cli->error("there was some kind of error with stomp: " . $e->getMessage());
-                $this->cli->message("going to sleep for 10, retrying then");
+                $log->error("there was some kind of error with stomp: " . $e->getMessage());
+                $log->info("going to sleep for 10, retrying then");
                 // we have a stomp exception here most likely that means that the server died.
                 // so we are going to sleep for a bit and retry
                 sleep(10);
@@ -86,24 +104,26 @@ class KingboardTask extends \King23\Tasks\King23Task
                 // @todo: check if that might cause open connections not to close over time
                 unset($stomp);
                 $stomp = new \Stomp($stompcfg['url'], $stompcfg['user'], $stompcfg['passwd']);
-                $stomp->subscribe($destination, array(
-                    "id" => $reg->stomp['dsub_id'], // dsub id, this one should be some unique identifier that identifies your board
-                                                    // multiple boards using the same dsub_id will consume each others subscription
-                    "persistent" => "true",         // this flag enables the dsub itself
-                    "ack" => "client",              // ensure we don't auto-ack (serverside) but have the client acknowledge his subscription
-                    "prefetch-count" => 1
-                ));
-                $this->cli->message("retrying");
+                $stomp->subscribe(
+                    $destination,
+                    array(
+                        "id" => $reg->stomp['dsub_id'],
+                        // dsub id, this one should be some unique identifier that identifies your board
+                        // multiple boards using the same dsub_id will consume each others subscription
+                        "persistent" => "true",
+                        // this flag enables the dsub itself
+                        "ack" => "client",
+                        // ensure we don't auto-ack (serverside) but have the client acknowledge his subscription
+                        "prefetch-count" => 1
+                    )
+                );
+                $log->info("stomp process retrying");
             }
         }
     }
-
-
-
 
     public function test(array $options)
     {
         var_dump(KillsByDayByEntity::mapReduce());
     }
-
 }
